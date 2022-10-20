66084adc580e41c5811bb77bfd082451c94bb1c0
Allen Chang <blue69221@gmail.com>
2022-10-20 14:34:35 +0800

    add filter

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -4,7 +4,7 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
+        if count % 1000 == 0: # 每 1000 筆時印出
             print(len(data))
 print(f'檔案讀取完了，總共有 {len(data)} 筆資料')
 
@@ -14,3 +14,12 @@ for d in data:
     # print(sum_len)
 
 print(f'留言的平均長度為 {sum_len/len(data)}')
+
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print(f'一共有 {len(new)} 筆留言長度小於 100')
+print(new[0])
+print(new[1])
